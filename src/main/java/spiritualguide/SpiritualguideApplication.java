6c23a141e53ef8bda345fc58c2e62af710f630a8
Luna <cocobuntus@gmail.com>
2026-06-16 16:25:06 +0200

    Initial commit

---
+++ src/main/java/spiritualguide/SpiritualguideApplication.java
@@ -0,0 +1,13 @@
+package spiritualguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritualguideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiritualguideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spiritualguide/SpiritualguideApplicationTests.java
@@ -0,0 +1,13 @@
+package spiritualguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiritualguideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
